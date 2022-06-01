1587824b55f1bef00dc7191e831b7887a08b96df
sahilkushwaha55/radon <sahilkushwaha55@gmail.com>
2022-06-01 16:06:34 +0530

    try to understand and change

---
+++ src/routes/myfunction.js
@@ -0,0 +1,8 @@
+const endpoint="www.functionup.org"
+
+const logs=function (){
+    console.log("this is from my function")
+}
+
+module.exports.abc=endpoint
+module.exports.funct=logs
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,9 +1,21 @@
 const express = require('express');
 
+const try1=require('./myfunction.js')
+
 const router = express.Router();
 
 router.get('/test-me', function (req, res) {
+    res.send("this file is access from"+ try1.abc)
+    try1.funct()
     res.send('My first ever api!')
+});
+
+router.get('/test-me2', function (req, res) {
+    res.send('My second api!')
+});
+
+router.get('/test-me3', function (req, res) {
+    res.send('My third api!')
 });
 
 module.exports = router;
