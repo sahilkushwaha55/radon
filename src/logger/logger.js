2a325451d223012d2eb15593e753806a73389bc0
sahilkushwaha55/radon <sahilkushwaha55@gmail.com>
2022-06-01 20:25:18 +0530

    changes depend on assignment 2

---
+++ src/logger/logger.js
@@ -0,0 +1,8 @@
+const trymybest="www.functionup.org"
+
+const welcome=function(){
+    console.log('Welcome to my application. I am Sahil and a part of FunctionUp Radon cohort')
+}
+
+module.exports.welcome=welcome
+module.exports.trymybest=trymybest
---
+++ src/util/helper.js
@@ -0,0 +1,14 @@
+const today= new Date()
+const printDate = function(){
+    console.log("DAY :" +today.getDate())
+}
+const printMonth=function(){
+    console.log("MONTH :" + today.getMonth())
+}
+const getBatchInfo=function(){
+    console.log("Radon, W2D2, the topic for today is Nodejs module system")
+}
+
+module.exports.printDate=printDate
+module.exports.printMonth=printMonth
+module.exports.getBatchInfo=getBatchInfo
