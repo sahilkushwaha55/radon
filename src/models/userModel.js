d29d69b5d08e5ece9d64f62c527054614192a809
sahilkushwaha55/radon <sahilkushwaha55@gmail.com>
2022-06-06 21:34:12 +0530

    book and author assignment

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,19 +1,10 @@
 const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema( {
-    firstName: String,
-    lastName: String,
-    mobile: {
-        type: String,
-        unique: true,
-        required: true
-    },
-    emailId: String,
-    gender: {
-        type: String,
-        enum: ["male", "female", "LGBTQ"] //"falana" will give an error
-    },
-    age: Number,
+const bookSchema = new mongoose.Schema( {
+    bookName: String,
+    authorName: String,
+    category : String,
+    year : Number,
     // isIndian: Boolean,
     // parentsInfo: {
     //     motherName: String,
@@ -23,7 +14,7 @@ const userSchema = new mongoose.Schema( {
     // cars: [ String  ]
 }, { timestamps: true });
 
-module.exports = mongoose.model('User', userSchema) //users
+module.exports = mongoose.model('book', bookSchema) //users
 
 
 
