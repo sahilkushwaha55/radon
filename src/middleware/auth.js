4d407a09078bf601bbf5c67f89d190bbbeb12eb8
sahilkushwaha55/radon <sahilkushwaha55@gmail.com>
2022-06-15 21:13:35 +0530

    save the new changes

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,12 +1,48 @@
-const authenticate = function(req, req, next) {
-    //check the token in request header
-    //validate this token
+const userModel = require('../models/userModel')
+const jwt = require('jsonwebtoken')
+
+
+const authenticate = async function(req, res, next) {
+
+    let token = req.headers["x-Auth-token"];
+    if (!token) token = req.headers["x-auth-token"];
+  
+    //If no token is present in the request header return error
+    if (!token) return res.send({ status: false, msg: "token must be present" });
+  
+    console.log(token);
+    
+    let decodedToken = jwt.verify(token, "functionup-thorium");
+    if (!decodedToken)
+      return res.send({ status: false, msg: "token is invalid" });
+  
+  
+  
+      let userId = req.params.userId;
+      let userDetails = await userModel.findById(userId);
+      if (!userDetails)
+        return res.send({ status: false, msg: "No such user exists" });
+  
 
     next()
 }
 
 
-const authorise = function(req, res, next) {
+const authorise = async function(req, res, next) {
     // comapre the logged in user's id and the id in request
+
+
+    let token = req.headers["x-Auth-token"];
+    if (!token) token = req.headers["x-auth-token"];
+    let decodedToken = jwt.verify(token, "functionup-thorium");
+    let userToBeModified = req.params.userId
+    //userId for the logged-in user
+    let userLoggedIn = decodedToken.userId
+
+    //userId comparision to check if the logged-in user is requesting for their own data
+    if(userToBeModified != userLoggedIn) return res.send({status: false, msg: 'User logged is not allowed to modify the requested users data'})
     next()
 }
+
+module.exports.authenticate = authenticate
+module.exports.authorise = authorise
